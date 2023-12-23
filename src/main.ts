f1a95139b981c95794d55ee82b9c78dd47529d0d
ShiKoLatA <sabouela@lakeheadu.ca>
2023-12-23 11:16:28 +0200

    feat: ng17 migration - standalone step 2 & 3

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,79 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { NgxMaskModule } from 'ngx-mask';
+import { MatSelectModule } from '@angular/material/select';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { StructureBikesEffects } from './store/structure-bikes.effects';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { bikesReducer } from './store/structure-bikes.reducer';
+import { StoreModule } from '@ngrx/store';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { withInterceptorsFromDi, provideHttpClient, HttpClient } from '@angular/common/http';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      MatTableModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatPaginatorModule,
+      MatSortModule,
+      StoreModule.forRoot({ bikes: bikesReducer }, {}),
+      StoreDevtoolsModule.instrument({
+        maxAge: 25, // Retains last 25 states
+        // logOnly: environment.production, // Restrict extension to log-only mode
+      }),
+      EffectsModule.forRoot([StructureBikesEffects]),
+      ReactiveFormsModule,
+      MatCardModule,
+      MatButtonModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatMenuModule,
+      MatDialogModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      }),
+      MatSelectModule,
+      NgxMaskModule.forRoot(),
+      SlickCarouselModule
+    ),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimations()
+  ]
+})
   .catch(err => console.error(err));
